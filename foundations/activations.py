2527abfe7961fd9634e122e2f2a084b3c657d7e7
Divyansh <Krrish020805@gmail.com>
2026-07-30 22:05:01 +0530

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -3,8 +3,7 @@ from numpy.typing import NDArray
 
 class Solution:
     def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        result = 1 / (1 + np.exp(-z))
-        return np.round(result, 5)
+        return np.round(1 / (1 + np.exp(-z)), 5)
 
     def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         return np.maximum(0, z)
