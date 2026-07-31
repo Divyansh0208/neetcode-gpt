6f84c9f3aae10c2959807a0b313afb7095c53585
Divyansh <Krrish020805@gmail.com>
2026-07-31 23:11:56 +0530

    Update Single neuron forward pass (Build a Neural Net 1/5)

--- foundations/neuron.py
+++ foundations/neuron.py
@@ -5,9 +5,7 @@ from numpy.typing import NDArray
 class Solution:
     def forward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, activation: str) -> float:
         z = np.dot(x, w) + b
-        if activation == "sigmoid":
-            ans = 1/(1+np.exp(-z))
-        elif activation == "relu":
-            ans = max(z,0)
-        
-        return float(np.round(ans, 5))
+        if activation == 'sigmoid':
+            return round(1/(1+np.exp(-z)), 5)
+        return round(max(0.0,z),5)
+        pass
