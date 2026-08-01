73a206995237f4bc33741814d142957896dd1eb7
Divyansh <Krrish020805@gmail.com>
2026-08-01 23:29:27 +0530

    Update RMS normalization (PyTorch 4/4)

--- model/rms_normalization.py
+++ model/rms_normalization.py
@@ -6,6 +6,6 @@ class Solution:
     def rms_norm(self, x: List[float], gamma: List[float], eps: float) -> List[float]:
         x = np.array(x)
         gamma = np.array(gamma)
-        rms = np.sqrt(np.mean(x ** 2) + eps)
-        x_hat = x / rms
+        root = np.sqrt(np.mean(x ** 2) + eps)
+        x_hat = x / root
         return np.round(gamma * x_hat, 4).tolist()
