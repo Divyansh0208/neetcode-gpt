d8c61afe8a50dc02008eded0f0474580fd4df50d
Divyansh <Krrish020805@gmail.com>
2026-08-04 23:54:48 +0530

    Update Word embeddings (NLP 1/4)

--- model/embeddings.py
+++ model/embeddings.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.typing import NDArray
 
+
 class Solution:
     def lookup(self, embeddings: NDArray[np.float64], token_ids: NDArray[np.int64]) -> NDArray[np.float64]:
-        return np.round(embeddings[token_ids], 5)
+        output = [embeddings[id] for id in token_ids]
+        return np.round(output, 5)
