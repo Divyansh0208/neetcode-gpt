c40743ba2b2e02b1e0208dd8b376a75040469b6e
Divyansh <Krrish020805@gmail.com>
2026-08-06 00:39:04 +0530

    Update NLP preprocessing (NLP 2/4)

--- data/nlp_preprocessing.py
+++ data/nlp_preprocessing.py
@@ -6,10 +6,10 @@ from typing import List
 class Solution:
     def get_dataset(self, positive: List[str], negative: List[str]) -> TensorType[float]:
         combined = positive + negative
+        dictionary = sorted({word for sentence in combined for word in sentence.split()})
 
-        vocabulary = sorted({word for sentence in combined for word in sentence.split()})
-        word_to_id = {word: idx + 1 for idx, word in enumerate(vocabulary)}
+        word_map = {dictionary[i]: float(i + 1) for i, word in enumerate(dictionary)}
+        
+        tensors = [torch.tensor([word_map[word] for word in sentence.split()]) for sentence in combined]
 
-        encoded = [torch.tensor([word_to_id[w] for w in s.split()]) for s in combined]
-
-        return nn.utils.rnn.pad_sequence(encoded, batch_first=True)
+        return nn.utils.rnn.pad_sequence(tensors, batch_first=True)
