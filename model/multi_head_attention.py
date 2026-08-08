7f52a53fec442fcc5d484afc15987a134e2bf878
Divyansh <Krrish020805@gmail.com>
2026-08-08 22:59:15 +0530

    Update Multi-headed self-attention (Attention & Transformers 2/3)

--- model/multi_head_attention.py
+++ model/multi_head_attention.py
@@ -7,17 +7,20 @@ class MultiHeadedSelfAttention(nn.Module):
     def __init__(self, embedding_dim: int, attention_dim: int, num_heads: int):
         super().__init__()
         torch.manual_seed(0)
-        self.att_heads = nn.ModuleList()
-        for i in range(num_heads):
-            self.att_heads.append(self.SingleHeadAttention(embedding_dim, attention_dim // num_heads))
-        self.output_proj = nn.Linear(attention_dim, attention_dim, bias=False)
+        head_size = attention_dim // num_heads
+        self.heads = nn.ModuleList([
+            self.SingleHeadAttention(embedding_dim, head_size) for _ in range(num_heads)
+        ])
+        
+        self.W_O = nn.Linear(attention_dim, attention_dim, bias=False)
 
     def forward(self, embedded: TensorType[float]) -> TensorType[float]:
-        head_outputs = []
-        for head in self.att_heads:
-            head_outputs.append(head(embedded))
-        concatenated = torch.cat(head_outputs, dim = 2)
-        return torch.round(self.output_proj(concatenated), decimals=4)
+        head_outputs = [head(embedded) for head in self.heads]
+        concat_out = torch.cat(head_outputs, dim=2)
+        
+        out = self.W_O(concat_out)
+        
+        return torch.round(out, decimals=4)
 
     class SingleHeadAttention(nn.Module):
         def __init__(self, embedding_dim: int, attention_dim: int):
@@ -32,7 +35,7 @@ class MultiHeadedSelfAttention(nn.Module):
             q = self.query_gen(embedded)
             v = self.value_gen(embedded)
 
-            scores = q @ torch.transpose(k, 1, 2) # @ is the same as torch.matmul()
+            scores = q @ torch.transpose(k, 1, 2)
             context_length, attention_dim = k.shape[1], k.shape[2]
             scores = scores / (attention_dim ** 0.5)
 
