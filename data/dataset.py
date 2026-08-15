7e5152e7a5c3ade5f5dbb6a35bceafdf4e0f7737
Divyansh <Krrish020805@gmail.com>
2026-08-16 00:10:09 +0530

    Update GPT dataset preparation (Build GPT 5/10)

--- data/dataset.py
+++ data/dataset.py
@@ -4,11 +4,14 @@ from typing import List, Tuple
 class Solution:
     def batch_loader(self, raw_dataset: str, context_length: int, batch_size: int) -> Tuple[List[List[str]], List[List[str]]]:
         torch.manual_seed(0)
-        tokenized = raw_dataset.split()
-        indices = torch.randint(low=0, high=len(tokenized) - context_length, size=(batch_size,)).tolist()
+        tokens = raw_dataset.split()
+        starts = torch.randint(0,len(tokens)-context_length,(batch_size,))
         X = []
         Y = []
-        for idx in indices:
-            X.append(tokenized[idx:idx+context_length])
-            Y.append(tokenized[idx+1:idx+1+context_length])
-        return X, Y
+
+        for i,start in enumerate(starts):
+            start = start.item()
+            X.append(tokens[start:start+context_length])
+            Y.append(tokens[start+1:start+1+context_length])
+        return (X,Y)
+
