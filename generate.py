c97494140a36f4d9b4af435ca2659701449cc4e4
Divyansh <Krrish020805@gmail.com>
2026-08-19 00:02:45 +0530

    Update Text generation (Build GPT 8/10)

--- generate.py
+++ generate.py
@@ -4,17 +4,17 @@ from torchtyping import TensorType
 
 class Solution:
     def generate(self, model, new_chars: int, context: TensorType[int], context_length: int, int_to_char: dict) -> str:
+
         generator = torch.manual_seed(0)
         initial_state = generator.get_state()
         result = []
-        for _ in range(new_chars):
-            if context.shape[1] > context_length:
-                context = context[:, -context_length:]
-            logits = model(context)
-            last_logits = logits[:, -1, :]
-            probs = nn.functional.softmax(last_logits, dim=-1)
-            next_token = torch.multinomial(probs, 1, generator=generator)
+        for i in range(new_chars):
+            current_context = context[:,-context_length:]        
+            logits = model(current_context)
+            preds = logits[-1]
+            probs = torch.softmax(logits, dim=-1).squeeze(0)
             generator.set_state(initial_state)
-            context = torch.cat((context, next_token), dim=-1)
-            result.append(int_to_char[next_token.item()])
-        return ''.join(result)
+            sample = torch.multinomial(probs, 1, generator=generator)
+            torch.cat((context,sample), dim=1)
+            result += int_to_char[sample.item()]
+        return "".join(result)
